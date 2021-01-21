2e6d605232687934bede4cc8b11ea3ba3c194a87
yutingzou <zouyuting0801@gmail.com>
2021-01-21 21:12:00 +0800

    edit Level 6 - Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
         myDuke.run();
     }
 
-    private void run() throws DukeException {
+    private void run() throws Exception {
         System.out.println("---------------------------------------------");
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println("---------------------------------------------");
@@ -25,132 +25,134 @@ public class Duke {
         taskList.add("todo");
         taskList.add("deadline");
         taskList.add("event");
+        taskList.add("list");
+        taskList.add("delete");
 
         while (input.hasNextLine()) {
             String s = input.nextLine();
-            if (s.toLowerCase().equals("bye")) {
-                System.out.println("---------------------------------------------");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("---------------------------------------------");
-                break;
-            } else if (s.toLowerCase().equals("list")) {
-                System.out.println("---------------------------------------------");
-                System.out.println("Here are the tasks in your list:");
-                int len = myList.size();
-                for (int i = 1; i < len + 1; i++) {
-                    Task curTask = myList.get(i - 1);
-                    System.out.println(i + "." + curTask);
-                }
-                System.out.println("---------------------------------------------");
-            } else {
-                String[] parts = s.split(" ", 2);
-                String taskType = parts[0];
+            String[] parts = s.split(" ", 2);
+            String taskType = parts[0];
 
-                if (parts.length == 1) {
-                    // exception
+            if(!taskList.contains(taskType.toLowerCase())){
+                System.out.println("---------------------------------------------");
+                System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                System.out.println("---------------------------------------------");
+            } else{
+                if (s.toLowerCase().equals("bye")) {
                     System.out.println("---------------------------------------------");
-                    System.out.println(parts[0]);
+                    System.out.println("Bye. Hope to see you again soon!");
+                    System.out.println("---------------------------------------------");
+                    break;
+                } else if (s.toLowerCase().equals("list")) {
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Here are the tasks in your list:");
+                    int len = myList.size();
+                    for (int i = 1; i < len + 1; i++) {
+                        Task curTask = myList.get(i - 1);
+                        System.out.println(i + "." + curTask);
+                    }
                     System.out.println("---------------------------------------------");
                 } else {
-
-//                if (s.substring(0, 4).toLowerCase().equals("done")) {
-                    if (taskType.toLowerCase().equals("done")) {
-//                    int taskNo = Integer.valueOf(s.substring(5, s.length()));
-                        // try {
-                        //if (taskNo <= myList.size()) {
-                        int taskNo = Integer.valueOf(parts[1]);
-                        Task curTask = myList.get(taskNo - 1);
-                        curTask.markAsDone();
-                        System.out.println("---------------------------------------------");
-                        System.out.println("Nice! I've marked this task as done: ");
-                        System.out.println(curTask);
-                        System.out.println("---------------------------------------------");
-                    }
-//                        } else {
-//                            Task newTask = new Task(s);
-//                            myList.add(newTask);
-//                            System.out.println("---------------------------------------------");
-//                            System.out.println("added: " + s);
-//                            System.out.println("---------------------------------------------");
-//
-//                    catch (DukeException e){
-//                        System.err.println(e.getLocalizedMessage());
-//                    }
-
-//                    catch (NumberFormatException e){
+//                    if (parts.length == 1) {
+//                        // exception
 //                        System.out.println("---------------------------------------------");
-//
-//                        throw new DukeException("OOPS! The task is invalid.", e);
-//                    }
-                    else {
-                        if (taskType.toLowerCase().equals("delete")) {
-                            //                    try{
+//                        System.out.println(parts[0]);
+//                        System.out.println("---------------------------------------------");
+//                    } else {
+                        if (taskType.toLowerCase().equals("done")) {
                             int taskNo = Integer.valueOf(parts[1]);
                             Task curTask = myList.get(taskNo - 1);
-                            myList.remove(taskNo-1);
+                            curTask.markAsDone();
                             System.out.println("---------------------------------------------");
-                            System.out.println("Noted. I've removed this task:");
+                            System.out.println("Nice! I've marked this task as done: ");
                             System.out.println(curTask);
-                            System.out.println("Now you have " + myList.size() + " tasks in the list.");
                             System.out.println("---------------------------------------------");
-                            //                    } catch (DukeException e){
-                            //                        System.err.println(e.getLocalizedMessage());
-                            //                    }
                         } else {
-                            if (taskType.toLowerCase().equals("todo")) {
-
-                                ToDo newTask = new ToDo(parts[1]);
-                                myList.add(newTask);
+                            if (taskType.toLowerCase().equals("delete")) {
+                                //                    try{
+                                int taskNo = Integer.valueOf(parts[1]);
+                                Task curTask = myList.get(taskNo - 1);
+                                myList.remove(taskNo - 1);
                                 System.out.println("---------------------------------------------");
-                                System.out.println("Got it. I've added this task: ");
-                                System.out.println("  " + newTask);
+                                System.out.println("Noted. I've removed this task:");
+                                System.out.println(curTask);
                                 System.out.println("Now you have " + myList.size() + " tasks in the list.");
                                 System.out.println("---------------------------------------------");
                             } else {
-                                if (taskType.toLowerCase().equals("deadline")) {
-                                    String task = parts[1];
-                                    String[] details = task.split(" /by ", 2);
-                                    String description = details[0];
-                                    String by = details[1];
-                                    Deadline newTask = new Deadline(description, by);
-
-                                    myList.add(newTask);
-                                    System.out.println("---------------------------------------------");
-                                    System.out.println("Got it. I've added this task: ");
-                                    System.out.println("  " + newTask);
-                                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
-                                    System.out.println("---------------------------------------------");
-                                } else {
-                                    if (taskType.toLowerCase().equals("event")) {
-                                        String task = parts[1];
-                                        String[] details = task.split(" /at ", 2);
-                                        String description = details[0];
-                                        String time = details[1];
-                                        Event newTask = new Event(description, time);
-
-                                        myList.add(newTask);
+                                if (taskType.toLowerCase().equals("todo")) {
+                                    try {
+                                        if (!parts[1].isBlank()) {
+                                            ToDo newTask = new ToDo(parts[1]);
+                                            myList.add(newTask);
+                                            System.out.println("---------------------------------------------");
+                                            System.out.println("Got it. I've added this task: ");
+                                            System.out.println("  " + newTask);
+                                            System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                                            System.out.println("---------------------------------------------");
+                                        } else {
+                                            throw new Exception("OOPS!!! The description of a todo cannot be empty.");
+                                        }
+                                    } catch(Exception e){
                                         System.out.println("---------------------------------------------");
-                                        System.out.println("Got it. I've added this task: ");
-                                        System.out.println("  " + newTask);
-                                        System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                                        System.out.println("OOPS!!! The description of a todo cannot be empty.");
                                         System.out.println("---------------------------------------------");
                                     }
-                                    //                            else{
-                                    //                                throw new Exception("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                                    //                            }
+
+                                } else {
+                                    if (taskType.toLowerCase().equals("deadline")) {
+                                        try{
+                                            if(!parts[1].isBlank()){
+                                                String task = parts[1];
+                                                String[] details = task.split(" /by ", 2);
+                                                String description = details[0];
+                                                String by = details[1];
+                                                Deadline newTask = new Deadline(description, by);
+
+                                                myList.add(newTask);
+                                                System.out.println("---------------------------------------------");
+                                                System.out.println("Got it. I've added this task: ");
+                                                System.out.println("  " + newTask);
+                                                System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                                                System.out.println("---------------------------------------------");
+                                            } else{
+                                                throw new Exception("OOPS!!! The description of a deadline cannot be empty.");
+                                            }
+                                        } catch (Exception e){
+                                            System.out.println("---------------------------------------------");
+                                            System.out.println("OOPS!!! The description of a deadline cannot be empty.");
+                                            System.out.println("---------------------------------------------");
+                                        }
+
+                                    } else {
+                                        if (taskType.toLowerCase().equals("event")) {
+                                            try{
+                                                if(!parts[1].isBlank()) {
+                                                    String task = parts[1];
+                                                    String[] details = task.split(" /at ", 2);
+                                                    String description = details[0];
+                                                    String time = details[1];
+                                                    Event newTask = new Event(description, time);
+
+                                                    myList.add(newTask);
+                                                    System.out.println("---------------------------------------------");
+                                                    System.out.println("Got it. I've added this task: ");
+                                                    System.out.println("  " + newTask);
+                                                    System.out.println("Now you have " + myList.size() + " tasks in the list.");
+                                                    System.out.println("---------------------------------------------");
+                                                } else{
+                                                    throw new Exception("OOPS!!! The description of an event cannot be empty.");
+                                                }
+                                            } catch (Exception e){
+                                                System.out.println("---------------------------------------------");
+                                                System.out.println("OOPS!!! The description of an event cannot be empty.");
+                                                System.out.println("---------------------------------------------");
+                                            }
+                                    }
                                 }
                             }
-
-//                    Task newTask = new Task(s);
-//                    myList.add(newTask);
-//                    System.out.println("---------------------------------------------");
-//                    System.out.println("added: " + s);
-//                    System.out.println("---------------------------------------------");
-
                         }
                     }
                 }
             }
         }
-    }
-}
+    }}
